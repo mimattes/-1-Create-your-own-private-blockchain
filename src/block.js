b2ac5fc5de960a195ea35b4dee99df02d8dcb9a4
mimattes <mmattes@gmx.net>
2021-11-28 20:27:55 +0100

    implementation of method stubs, no testing yet

--- src/block.js
+++ src/block.js
@@ -1,11 +1,11 @@
 /**
  *                          Block class
- *  The Block class is a main component into any Blockchain platform, 
+ *  The Block class is a main component into any Blockchain platform,
  *  it will store the data and act as a dataset for your application.
  *  The class will expose a method to validate the data... The body of
  *  the block will contain an Object that contain the data to be stored,
  *  the data should be stored encoded.
- *  All the exposed methods should return a Promise to allow all the methods 
+ *  All the exposed methods should return a Promise to allow all the methods
  *  run asynchronous.
  */
 
@@ -22,7 +22,7 @@ class Block {
 		this.time = 0;                                              // Timestamp for the Block creation
 		this.previousBlockHash = null;                              // Reference to the previous Block Hash
     }
-    
+
     /**
      *  validate() method will validate if the block has been tampered or not.
      *  Been tampered means that someone from outside the application tried to change
@@ -38,33 +38,48 @@ class Block {
     validate() {
         let self = this;
         return new Promise((resolve, reject) => {
-            // Save in auxiliary variable the current block hash
-                                            
-            // Recalculate the hash of the Block
-            // Comparing if the hashes changed
+          // Save in auxiliary variable the current block hash
+          let storedHash = self.hash;
+          // Recalculate the hash of the Block
+          self.hash = null;
+          let recalc = SHA256(JSON.stringify(self)).toString();
+          // Comparing if the hashes changed
+          if (storedHash !== recalc) {
             // Returning the Block is not valid
-            
+            return false;
+          } else {
             // Returning the Block is valid
-
+            return true;
+          }
         });
     }
+
 
     /**
      *  Auxiliary Method to return the block body (decoding the data)
      *  Steps:
-     *  
+     *
      *  1. Use hex2ascii module to decode the data
      *  2. Because data is a javascript object use JSON.parse(string) to get the Javascript Object
-     *  3. Resolve with the data and make sure that you don't need to return the data for the `genesis block` 
+     *  3. Resolve with the data and make sure that you don't need to return the data for the `genesis block`
      *     or Reject with an error.
      */
     getBData() {
+      let self = this;
+      return new Promise((resolve, reject) => {
         // Getting the encoded data saved in the Block
+        let data = self.data;
         // Decoding the data to retrieve the JSON representation of the object
+        let decoded = hex2ascii(data);
         // Parse the data to an object to be retrieve.
-
+        let parsed = JSON.parse(decoded);
         // Resolve with the data if the object isn't the Genesis block
-
+        if ( parsed !== 'Genesis Block') {
+          resolve(parsed)
+        } else {
+          reject("this is the Genesis Block");
+        }
+      });
     }
 
 }
