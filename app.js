a0133c6c3d9503f1a089d39bbad5e885b288e6fc
mimattes <mmattes@gmx.net>
2021-12-07 19:22:42 +0100

    replaced console.log by log from debug package

--- app.js
+++ app.js
@@ -9,6 +9,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const log = require('debug')('http');
+
 /**
  * Require the Blockchain class. This allow us to have only one instance of the class.
  */
@@ -48,7 +50,7 @@ class ApplicationServer {
 	start() {
 		let self = this;
 		this.app.listen(this.app.get("port"), () => {
-			console.log(`Server Listening for port: ${self.app.get("port")}`);
+			log(`Server Listening for port: ${self.app.get("port")}`);
 		});
 	}
 
